403cbbaaac3017006a0da23d89586e6edde603eb
Ricky Salinas <rsalinas1@mail.usf.edu>
2014-04-29 02:59:10 -0400

    Allow disabling the IME selector notification (1/2)
    Credit CM
    
    Signed-off-by: Ricky Salinas <rsalinas1@mail.usf.edu>
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2649,6 +2649,12 @@ public final class Settings {
         public static final String VOLUME_KEYS_CONTROL_RING_STREAM = "volume_keys_control_ring_stream";
 
         /**
+         * Whether to show the IME switcher in the status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_IME_SWITCHER = "status_bar_ime_switcher";
+
+        /**
          * Whether to prevent loud volume levels when headset is first plugged in.
          * @hide
          */
