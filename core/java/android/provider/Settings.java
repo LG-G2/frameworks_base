302d99a120738aa8370b45fb8daf7a3a79c917c5
benjamingoodwin <benjamin.j.goodwin@gmail.com>
2014-05-18 15:50:43 +0100

    QuickToggles from PA - Initial Commit

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2416,6 +2416,12 @@ public final class Settings {
         public static final String EGG_MODE = "egg_mode";
 
         /**
+         * Quick settings tiles to show on status bar
+         * @hide
+         */
+        public static final String QUICK_SETTINGS_TILES = "quick_settings_tiles";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
