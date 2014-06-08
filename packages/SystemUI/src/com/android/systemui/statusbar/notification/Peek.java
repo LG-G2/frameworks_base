7e5978f6a7dc5b1dc58bfae5962ec5bc3013f1f8
benjamingoodwin <benjamin.j.goodwin@gmail.com>
2014-06-08 23:06:23 +0100

    Revert "PEEK: configurable pickup timeout (1/2)"
    This reverts commit 9c9d7f69fb3dbd7a8f8e250254433867055e8025.

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/Peek.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/Peek.java
@@ -72,10 +72,9 @@ public class Peek implements SensorActivityHandler.SensorChangedCallback {
 
     private static final float ICON_LOW_OPACITY = 0.3f;
     private static final int NOTIFICATION_PEEK_TIME = 7000; // 7 secs
+    private static final int PARTIAL_WAKELOCK_TIME = 15000; // 15 secs
     private static final long SCREEN_ON_START_DELAY = 300; // 300 ms
     private static final long REMOVE_VIEW_DELAY = 300; // 300 ms
-
-    private int mPeekPickupTimeout;
 
     private BaseStatusBar mStatusBar;
 
@@ -388,9 +387,7 @@ public class Peek implements SensorActivityHandler.SensorChangedCallback {
                 }
 
                 mWakeLockHandler.removeCallbacks(mPartialWakeLockRunnable);
-                mPeekPickupTimeout = Settings.Secure.getIntForUser(mContext.getContentResolver(),
-                        Settings.System.PEEK_PICKUP_TIMEOUT, 0, UserHandle.USER_CURRENT);
-                mWakeLockHandler.postDelayed(mPartialWakeLockRunnable, mPeekPickupTimeout);
+                mWakeLockHandler.postDelayed(mPartialWakeLockRunnable, PARTIAL_WAKELOCK_TIME);
 
                 mNextNotification = n;
                 return;
