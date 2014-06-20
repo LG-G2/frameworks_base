ae40c79c851752f8b8f4940873bfbbea153b6e9c
Adnan <adnan@cyngn.com>
2014-06-20 16:51:49 -0700

    SystemUI: Expose heads up.
    Signed-off-by: Roman Birg <roman@cyngn.com>
    Signed-off-by: Adnan Begovic <adnan@cyngn.com>
    Change-Id: I276cb1c80d392b7dbac28e5f553207415b558481
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SettingsProvider/res/values/defaults.xml
    	packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2550,6 +2550,20 @@ public final class Settings {
         public static final String SIP_CALL_OPTIONS = "sip_call_options";
 
         /**
+         * Heads Up Notifications
+         *
+         * @hide
+         */
+        public static final String HEADS_UP_NOTIFICATION = "heads_up_enabled";
+
+        /**
+         * Which applications to disable heads up notifications for
+         *
+         * @hide
+         */
+        public static final String HEADS_UP_CUSTOM_VALUES = "heads_up_custom_values";
+
+        /**
          * One of the sip call options: Always use SIP with network access.
          * @hide
          */
